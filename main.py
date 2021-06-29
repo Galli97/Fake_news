706ea5ac5e64bef3c3a8acc3a67a9112a719f216
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-06-29 16:47:30 +0200

    main5

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ if solver.net.use_tf_threading:
 
 
 im1=util.random_crop(im1,[128,128])
-exif_lbl = np.array(['a','b','c','d','e'])
+exif_lbl = np.array(['1','0','1','0','0'])
 exif_lbl = np.transpose(exif_lbl)
+
 
 im1_merge = {'im_a':[im1,im1,im1,im1,im1,im1],'im_b':[im1,im1,im1,im1,im1,im1],'exif_lbl': exif_lbl}
 exif_solver.ExifSolver.setup_data(solver,im1,im1_merge)
