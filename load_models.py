f114fcf1a490c042c9179c8d7799d9892dad0d5d
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-06-30 18:53:24 +0200

    mainrr

--- load_models.py
+++ load_models.py
@@ -10,6 +10,7 @@ def initialize_exif(ckpt='', init=True, use_gpu=0):
                 'train_classifcation':True,
                 'freeze_base': True,
                 'im_size':128,
+                'train_runner'=train_runner
                 'batch_size':64,
                 'use_gpu':[use_gpu],
                 'use_tf_threading':False, ###era false
