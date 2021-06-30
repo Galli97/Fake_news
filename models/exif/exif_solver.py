3038482d276c93d7eafa5360c1b1f752fafb3e82
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-06-30 18:34:03 +0200

    main70

--- models/exif/exif_solver.py
+++ models/exif/exif_solver.py
@@ -159,7 +159,7 @@ class ExifSolver(object):
 
             if self.i % self.save_iter == 0 and self.i != self.start_i:
                 io.make_ckpt(self.saver, self.sess, self.ckpt_path, self.i)
-        if self.i = self.train_iterations
+        if self.i == self.train_iterations:
            self.use_exif_summary == True
            self.test(writer=self.test_writer)
         return
