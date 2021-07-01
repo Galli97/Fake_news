37ecdae953741e43ee12abf562695a4ad296ec58
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-07-01 17:40:08 +0200

    ref

--- main.py
+++ main.py
@@ -19,11 +19,7 @@ print("---------------------------------------------------------------------")
 
 solver = initialize_exif()
 
-tf.compat.v1.train.start_queue_runners(sess=solver.sess, coord=solver.coord)
 solver.sess.run(tf.compat.v1.global_variables_initializer())
-solver.coord = tf.compat.v1.train.Coordinator()
-solver.net.train_runner.start_p_threads(solver.sess)
-
 
 if solver.net.use_tf_threading:
     solver.coord = tf.train.Coordinator()
