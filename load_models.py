4b169992f670420e0b09ac8a828efa915eebc911
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-07-05 11:32:38 +0200

    hh

--- load_models.py
+++ load_models.py
@@ -5,18 +5,18 @@ import tensorflow as tf
 def initialize_exif(ckpt='', init=True, use_gpu=0):
     from models.exif import exif_net, exif_solver
     tf.compat.v1.reset_default_graph()
-    net_args = {'num_classes':80+3,
+    net_args = {'num_classes':71, #71
                 'is_training':False,
                 'train_classifcation':True,
                 'freeze_base': True,
                 'im_size':128,
                 'batch_size':64,
                 'use_gpu':[use_gpu],
-                'use_tf_threading':False, ###era false
+                'use_tf_threading':False,
                 'learning_rate':1e-4}
 
     solver = exif_solver.initialize({'checkpoint':ckpt,
-                                     'use_exif_summary':False, ###era false
+                                     'use_exif_summary':False,
                                      'init_summary':True,
                                      'exp_name':'eval'})
     if init:
