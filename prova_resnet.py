a2e8d33591ef4a7ad23e2cd97a54341b377221f0
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-08-29 19:48:18 +0200

    c

--- prova_resnet.py
+++ prova_resnet.py
@@ -15,7 +15,7 @@ import cv2
 
 import numpy as np
 
-def create_base_model(image_shape, dropout_rate, suffix=''):
+def create_base_model(image_shape, dropout_rate, suffix):
     I1 = Input(shape=image_shape)
     model = ResNet50(include_top=False, weights='imagenet', input_tensor=I1, pooling=None)
     model.layers.pop()
@@ -41,10 +41,10 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
 def create_siamese_model(image_shape, dropout_rate):
 
     output_left, input_left = create_base_model(image_shape, dropout_rate)
-    #output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
+    output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
 
     L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
-    L1_distance = L1_layer([output_left, output_left])
+    L1_distance = L1_layer([output_left, output_right])
     L1_prediction = Dense(1, use_bias=True,
                           activation='sigmoid',
                           kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
@@ -52,7 +52,7 @@ def create_siamese_model(image_shape, dropout_rate):
 
     prediction = Dropout(0.2)(L1_prediction)
 
-    siamese_model = Model(inputs=[input_left, input_left], outputs=prediction)
+    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
 
     return siamese_model
 
