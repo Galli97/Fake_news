649ea5d3815b8bd96e88181da8227372151554f6
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-08-29 19:10:13 +0200

    nb

--- prova_resnet.py
+++ prova_resnet.py
@@ -4,15 +4,15 @@ import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras import Input
 from keras import Model
-from keras.layers import Dense,Flatten,Dropout
+from keras.initializers import RandomNormal
+from keras.layers import Dense,Flatten,Dropout,Lambda
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import load_model
 from extract_exif import extract_exif, random_list,generate_label,cropping_list,get_np_arrays
+from matplotlib import image
 import pickle
+
 import numpy as np
-
-
-
 
 def create_base_model(image_shape, dropout_rate, suffix=''):
     I1 = Input(shape=image_shape)
@@ -29,8 +29,11 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
 
     x = model.output
     x = Flatten(name=flatten_name)(x)
-    x = Dense(4096, activation='relu')(x)
-    
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(dropout_rate)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(dropout_rate)(x)
+
     return x, model.input
 
 
@@ -39,52 +42,42 @@ def create_siamese_model(image_shape, dropout_rate):
     output_left, input_left = create_base_model(image_shape, dropout_rate)
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
 
-    L1_prediction = [output_left,output_right]
+    L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
+    L1_distance = L1_layer([output_left, output_right])
+    L1_prediction = Dense(1, use_bias=True,
+                          activation='sigmoid',
+                          kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
+                          name='weighted-average')(L1_distance)
 
-    #L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
-    #L1_distance = L1_layer([output_left, output_right])
-    #L1_prediction = Dense(1, use_bias=True,
-    #                      activation='sigmoid',
-    #                      kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
-    #                      name='weighted-average')(L1_distance)
+    prediction = Dropout(0.2)(L1_prediction)
 
-    siamese_model = Model(inputs=[input_left, input_right], outputs=L1_prediction)
+    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
 
     return siamese_model
 
-
-
-siamese_model = create_siamese_model(image_shape=(128, 128, 3),dropout_rate=0.2)
+siamese_model = create_siamese_model(image_shape=(64, 64, 3),
+                                         dropout_rate=0.2)
 
 siamese_model.compile(loss='binary_crossentropy',
                       optimizer=Adam(lr=0.0001),
                       metrics=['binary_crossentropy', 'acc'])
-
-#with open("exif_lbl.txt", "rb") as fp:   #Picklingpickle.dump(l, fp)
-#	exif_lbl = pickle.load(fp)
-#fp.close()
-
-list1,list2 = get_np_arrays('cropped_arrays.npy')
-X_train = list1
-Y_train = list2
-#cls_lbl= len(exif_lbl)
-training_generator = tf.keras.ImageDataGenerator(rotation_range=20, zoom_range=0.15,
-	width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15,
-	horizontal_flip=True, fill_mode="nearest")
- 
-
-
-siamese_model.fit_generator(training_generator.tf.keras.flow(X_train,Y_train,batch_size=64),steps_per_epoch=1000,epochs=10,validation_data=training_generator)
+imagexs = image.imread('D01_img_orig_0001.jpg')
+imagexs= np.asarray(imagexs)
+x = [imagexs]
+y = [imagexs]
+siamese_model.fit(x,y,#steps_per_epoch=1000,
+                            epochs=10,
+                            verbose=1,
                             #callbacks=[checkpoint, tensor_board_callback, lr_reducer, early_stopper, csv_logger],
-                            #validation_data=validation_data,
+                            validation_data=(imagexs,imagexs))
+                            #max_q_size=3)
 
 siamese_model.save('siamese_model.h5')
 
 
-
+"""
 # and the my prediction
 siamese_net = load_model('siamese_model.h5', custom_objects={"tf": tf})
-"""
 X_1 = [image, ] * len(markers)
 batch = [markers, X_1]
 result = siamese_net.predict_on_batch(batch)
@@ -92,4 +85,5 @@ result = siamese_net.predict_on_batch(batch)
 markers = [image]
 X_1 = [image, ] * len(markers)
 batch = [markers, X_1]
-result = siamese_net.predict_on_batch(batch)"""
+result = siamese_net.predict_on_batch(batch)
+"""
