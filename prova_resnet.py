a35522a4e5ed08a0680e271c81c314d9b8272aee
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-08-30 12:15:05 +0200

    cvcv

--- prova_resnet.py
+++ prova_resnet.py
@@ -50,13 +50,13 @@ def create_siamese_model(image_shape, dropout_rate):
     # L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
     # L1_distance = L1_layer([output_left, output_right])
     L1_prediction = Dense(1, use_bias=True,
-                          # activation='sigmoid',
-                          # input_shape = image_shape,
-                          # kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
-                          # name='weighted-average')(outputs)
+                          activation='sigmoid',
+                          input_shape = image_shape,
+                          kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
+                          name='weighted-average')(outputs)
     prediction = Dropout(0.2)(L1_prediction)
 
-    siamese_model = Model(inputs=[input_left, input_right], outputs=[output_left, output_right])
+    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
 
     return siamese_model
 
