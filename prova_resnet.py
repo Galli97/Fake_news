d964459511f721d13e4d91dca41941531dfeeb63
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-08-30 11:56:49 +0200

    nn

--- prova_resnet.py
+++ prova_resnet.py
@@ -1,9 +1,9 @@
+from re import I
 from models import exif
 from PIL import Image
 import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras import Input
-from tensorflow.keras import Sequential
 from keras import Model
 from keras.initializers import RandomNormal
 from keras.layers import Dense,Flatten,Dropout,Lambda
@@ -11,14 +11,12 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import load_model
 from extract_exif import extract_exif, random_list,generate_label,cropping_list,get_np_arrays
 from matplotlib import image
-import pickle
+from lib.utils import benchmark_utils, util,io
 import cv2
-
 import numpy as np
 
 def create_base_model(image_shape, dropout_rate, suffix=''):
-    I1 = Input(shape=image_shape)
-    model = Sequential()
+    I1 = Input(image_shape)
     model = ResNet50(include_top=False, weights='imagenet', input_tensor=I1, pooling=None)
     model.layers.pop()
     model.outputs = [model.layers[-1].output]
@@ -30,7 +28,7 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
 
     flatten_name = 'flatten' + str(suffix)
 
-    x = model.outputs
+    x = model.output
     x = Flatten(name=flatten_name)(x)
     x = Dense(1024, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
@@ -44,36 +42,42 @@ def create_siamese_model(image_shape, dropout_rate):
 
     output_left, input_left = create_base_model(image_shape, dropout_rate)
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
-
+    print("------------------------------------------------------------------------------")
+    print(output_left)
+    print("------------------------------------------------------------------------------")
+    output = tf.concat([output_left,output_right],0)
+    """
     L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
     L1_distance = L1_layer([output_left, output_right])
     L1_prediction = Dense(1, use_bias=True,
                           activation='sigmoid',
+                          input_shape = image_shape,
                           kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
                           name='weighted-average')(L1_distance)
-
     prediction = Dropout(0.2)(L1_prediction)
-
-    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
+"""
+    siamese_model = Model(inputs=[input_left, input_right], outputs=output)
 
     return siamese_model
 
-siamese_model = create_siamese_model(image_shape=(32, 32, 3),
+siamese_model = create_siamese_model(image_shape=(64, 64, 3),
                                          dropout_rate=0.2)
 
 siamese_model.compile(loss='binary_crossentropy',
                       optimizer=Adam(lr=0.0001),
                       metrics=['binary_crossentropy', 'acc'])
-imagexs = image.imread('D01_img_orig_0001.jpg')
-imagexs=cv2.resize(imagexs, (32, 32))
-imagexs= np.asarray(imagexs)
-x = [imagexs]
-y = [imagexs]
-siamese_model.fit(x,#steps_per_epoch=1000,
-                            epochs=10,
-                            verbose=1,
+
+imagexs =cv2.imread('D01_img_orig_0001.jpg')[:,:,[2,1,0]]
+
+imagexs = np.array(imagexs,np.float32)
+imagexs = util.random_crop(imagexs,[64,64])
+imagexs = np.expand_dims(imagexs,axis=0)
+
+siamese_model.fit(x=(imagexs,imagexs),y=(imagexs),batch_size = 32,#steps_per_epoch=1000,
+                            epochs=10)
+                            #verbose=1,
                             #callbacks=[checkpoint, tensor_board_callback, lr_reducer, early_stopper, csv_logger],
-                            validation_data=x)
+                            #validation_data=(imagexs,imagexs))
                             #max_q_size=3)
 
 siamese_model.save('siamese_model.h5')
