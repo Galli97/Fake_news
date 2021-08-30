e509bd02a40c34bdd6ebbd4eae62492b1d419042
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-08-30 12:10:45 +0200

    c

--- prova_resnet.py
+++ prova_resnet.py
@@ -47,25 +47,25 @@ def create_siamese_model(image_shape, dropout_rate):
     print("------------------------------------------------------------------------------")
     #output = tf.concat([output_left,output_right],0)
     
-    L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
-    L1_distance = L1_layer([output_left, output_right])
-    L1_prediction = Dense(1, use_bias=True,
-                          activation='sigmoid',
-                          input_shape = image_shape,
-                          kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
-                          name='weighted-average')(L1_distance)
-    prediction = Dropout(0.2)(L1_prediction)
+    # L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
+    # L1_distance = L1_layer([output_left, output_right])
+    # L1_prediction = Dense(1, use_bias=True,
+                          # activation='sigmoid',
+                          # input_shape = image_shape,
+                          # kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
+                          # name='weighted-average')(L1_distance)
+    # prediction = Dropout(0.2)(L1_prediction)
 
-    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
+    siamese_model = Model(inputs=[input_left, input_right], outputs=[output_left, output_right])
 
     return siamese_model
 
 siamese_model = create_siamese_model(image_shape=(64, 64, 3),
                                          dropout_rate=0.2)
 
-siamese_model.compile(loss='binary_crossentropy',
-                      optimizer=Adam(lr=0.0001),
-                      metrics=['binary_crossentropy', 'acc'])
+# siamese_model.compile(loss='binary_crossentropy',
+                      # optimizer=Adam(lr=0.0001),
+                      # metrics=['binary_crossentropy', 'acc'])
 
 imagexs =cv2.imread('D01_img_orig_0001.jpg')[:,:,[2,1,0]]
 
@@ -73,7 +73,7 @@ imagexs = np.array(imagexs,np.float32)
 imagexs = util.random_crop(imagexs,[64,64])
 imagexs = np.expand_dims(imagexs,axis=0)
 
-siamese_model.fit(x=(imagexs,imagexs),y=(imagexs),batch_size = 32,#steps_per_epoch=1000,
+siamese_model.fit(x=(imagexs,imagexs),y=(imagexs),batch_size = None,#steps_per_epoch=1000,
                             epochs=10)
                             #verbose=1,
                             #callbacks=[checkpoint, tensor_board_callback, lr_reducer, early_stopper, csv_logger],
