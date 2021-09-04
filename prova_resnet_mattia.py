6e730316c3b3cecdc6646774e2763f9e154eab78
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-04 17:36:29 +0200

    hvjhj

--- prova_resnet_mattia.py
+++ prova_resnet_mattia.py
@@ -138,6 +138,7 @@ x_train = datagenerator(list1,exif_lbl,32)
                             # 
 imagexs = np.expand_dims(list1[0],axis=0)
 imagexs2 = np.expand_dims(list2[0],axis=0)
+imagexs=tf.stack(imagexs,imagexs2)
+label=generate_label(imagexs2)
 
-
-siamese_model.fit(x = (imagexs,imagexs2),y = imagexs2,epochs=10)
+siamese_model.fit(x = (imagexs,imagexs2),y = label,epochs=10)
