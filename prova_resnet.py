02203de20738d15ce84718ce21228d44389d74e9
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-05 15:03:21 +0200

    e

--- prova_resnet.py
+++ prova_resnet.py
@@ -55,8 +55,7 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
     x = Dense(4096, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
     
-    print('x shape=')
-    print(x.shape)
+    
     #x = Dense(512, activation='relu')(x)
     #x = Dropout(dropout_rate)(x)
 
@@ -65,20 +64,33 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
 
 def create_siamese_model(image_shape, dropout_rate):
 
+    num_classes=71;
     output_left, input_left = create_base_model(image_shape, dropout_rate)
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
-    #output = tf.concat([output_left,output_right],0)
+    output = tf.concat([output_left,output_right],0)
     
-    L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
-    L1_distance = L1_layer([output_left, output_right])
-    L1_prediction = Dense(1, use_bias=True,
-                          activation='sigmoid',
-                          input_shape = image_shape,
-                          kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
-                          name='weighted-average')(L1_distance)
+    # Set the input shape
+    input_shape = (output.shape)
 
-    prediction = Dropout(0.2)(L1_prediction)
+    # Create the model
+    model2 = Sequential()
+    model2.add(Dense(4096, input_shape=input_shape, activation='relu'))
+    model2.add(Dense(2048, activation='relu'))
+    model2.add(Dense(1024, activation='relu'))
+    model2.add(Dense(num_classes, activation='softmax'))
+    
+    # L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
+    # L1_distance = L1_layer([output_left, output_right])
+    # L1_prediction = Dense(1, use_bias=True,
+                          # activation='sigmoid',
+                          # input_shape = image_shape,
+                          # kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
+                          # name='weighted-average')(L1_distance)
 
+    # prediction = Dropout(0.2)(L1_prediction)
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, Y_train, epochs=10, batch_size=250, verbose=1, validation_split=0.2)
     siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
 
     return siamese_model
