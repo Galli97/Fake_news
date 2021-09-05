db23a3ba136ded34f3a44faaeada89da9b6d2094
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-05 16:36:55 +0200

    n

--- prova_resnet.py
+++ prova_resnet.py
@@ -61,12 +61,6 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
     x = model.output
     x = Flatten(name=flatten_name)(x)
     x = Dense(4096, activation='relu')(x)
-    # x = Flatten(name=flatten_name)(x)
-    # x = Dropout(dropout_rate)(x)
-    
-    
-    #x = Dense(512, activation='relu')(x)
-    #x = Dropout(dropout_rate)(x)
 
     return x, model.input
 
@@ -78,17 +72,9 @@ def create_siamese_model(image_shape, dropout_rate):
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
     
     output = tf.concat([output_left,output_right],1)
-    # L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
-    # L1_distance = L1_layer([output_left, output_right])
-    # L1_prediction = Dense(1, use_bias=True,
-                          # activation='sigmoid',
-                          # input_shape = image_shape,
-                          # kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
-                          # name='weighted-average')(L1_distance)
-
-    # prediction = Dropout(0.2)(L1_prediction)
     print('Dimensione in output')
     print(output.shape)
+    
     siamese_model = Model(inputs=[input_left, input_right], outputs=output)
 
     return siamese_model,output
@@ -166,7 +152,7 @@ siamese_model.compile(loss='binary_crossentropy',
                       optimizer=Adam(lr=0.0001),
                       metrics=['binary_crossentropy', 'acc'])
                       
-siamese_model.fit(x = (imagexs,imagexs2),y = output_siamese,epochs=10)
+# siamese_model.fit(x = (imagexs,imagexs2),y = output_siamese,epochs=10)
 
 mlp_model=create_mlp(output_siamese.shape)
 
