0a4d33677ae2c7cc8e21c1c140c2c2223ea58ae5
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-05 15:16:14 +0200

    v

--- prova_resnet.py
+++ prova_resnet.py
@@ -21,7 +21,7 @@ import pickle
 list1,list2 = get_np_arrays('cropped_arrays.npy')
 imagexs = np.expand_dims(list1[0],axis=0)
 imagexs2 = np.expand_dims(list2[0],axis=0)
-imagexs=tf.stack([imagexs,imagexs2],axis=0)
+# imagexs=tf.stack([imagexs,imagexs2],axis=0)
 with open("exif_lbl.txt", "rb") as fp:   #Picklingpickle.dump(l, fp)
 	exif_lbl = pickle.load(fp)
 fp.close()
