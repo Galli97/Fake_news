de4375e66beea4fb165ea44c648a6ca978797b31
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-05 14:36:35 +0200

    xcx

--- prova_resnet.py
+++ prova_resnet.py
@@ -138,10 +138,10 @@ x_train = datagenerator(list1,exif_lbl,32)
                             # 
 imagexs = np.expand_dims(list1[0],axis=0)
 imagexs2 = np.expand_dims(list2[0],axis=0)
-imagexs=tf.stack([imagexs,imagexs2],axis=0)
+#imagexs=tf.stack([imagexs,imagexs2],axis=0)
 label=np.zeros(len(exif_lbl));
 # for i in range(len(exif_lbl)):
        # label[i]=[exif_lbl[i]]
    
     
-siamese_model.fit(x = imagexs,y = imagexs2,epochs=10)
+siamese_model.fit(x = imagexs,y = exif_lbl[0],epochs=10)
