29f21bda1cabe522b52c5fbbe5c7d20335d85113
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-06 16:11:02 +0200

    x

--- prova_exif.py
+++ prova_exif.py
@@ -140,4 +140,4 @@ imagexs = np.expand_dims(list1[0],axis=0)
 imagexs2 = np.expand_dims(list2[0],axis=0)
 imagexs=tf.stack([imagexs,imagexs2],axis=0)
 
-siamese_model.fit(x = imagexs,y = exif_lbl[0],epochs=10)
+siamese_model.fit(x = np.array(imagexs),y = np.array(exif_lbl[0]),epochs=10)
