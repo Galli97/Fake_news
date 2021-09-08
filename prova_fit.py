188eeb6591005bbc2e7a1ef53c55c0b0cdea953a
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-08 16:55:25 +0200

    n,

--- prova_fit.py
+++ prova_fit.py
@@ -129,7 +129,6 @@ fp.close()
 #######################################################################################à
 list1,list2 = get_np_arrays('cropped_arrays.npy')
 
-x_train = datagenerator(list1,exif_lbl,32)
 
 imagexs = np.expand_dims(list1[0],axis=0)
 imagexs2 = np.expand_dims(list2[0],axis=0)
