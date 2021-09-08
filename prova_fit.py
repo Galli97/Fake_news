a1dee8f68326313881945f8fffbcee664bd36121
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-08 15:35:53 +0200

    xvc

--- prova_fit.py
+++ prova_fit.py
@@ -94,9 +94,9 @@ def create_siamese_model(image_shape, dropout_rate):
 siamese_model = create_siamese_model(image_shape=(128,128, 3),
                                          dropout_rate=0.2)
 
-siamese_model.compile(loss='categorical_crossentropy',
+siamese_model.compile(loss='sparse_categorical_crossentropy',
                       optimizer=Adam(lr=0.0001),
-                      metrics=['categorical_crossentropy', 'acc'])
+                      metrics=['sparse_categorical_crossentropy', 'acc'])
 
 with open("exif_lbl.txt", "rb") as fp:   #Picklingpickle.dump(l, fp)
 	exif_lbl = pickle.load(fp)
