738aee24cdc06c34aa318cbbd40cef5635f4df50
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-08 14:58:23 +0200

    vzc

--- prova_fit.py
+++ prova_fit.py
@@ -84,7 +84,7 @@ fp.close()
 list1,list2 = get_np_arrays('cropped_arrays.npy')
 
 le = preprocessing.LabelEncoder()
-le.fit(exif_lbl[324])
+le.fit(exif_lbl[30])
 print(le.classes_)
 imagexs = np.expand_dims(list1[0],axis=0)
 imagexs2 = np.expand_dims(list2[0],axis=0)
