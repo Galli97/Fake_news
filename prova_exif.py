c830100c18b78ebdaf7f669afe6899e07fbeea47
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-09 12:40:31 +0200

    bk

--- prova_exif.py
+++ prova_exif.py
@@ -203,7 +203,7 @@ x1_train = np.reshape(x1_train, (-1, 16384)) #(128x128x3)
 # prepare input data
 X_train_enc = prepare_inputs(x1_train)
 # prepare output data
-y_train=labels
+y_train=exif_lbl
 y_train=np.reshape(y_train, (-1, 273421))
 y_train_enc = prepare_targets(y_train)
 #imagexs =cv2.imread('D01_img_orig_0001.jpg')
