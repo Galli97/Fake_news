103a73bcc9f2c7ed6462a8c51542331be0396844
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-20 11:35:00 +0200

    m

--- prova_exif.py
+++ prova_exif.py
@@ -115,7 +115,7 @@ def create_siamese_model(image_shape, dropout_rate):
     
     L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
     L1_distance = L1_layer([output_left, output_right])
-    L1_prediction = Dense(1, use_bias=True,
+    L1_prediction = Dense(71, use_bias=True,
                           activation='sigmoid',
                           input_shape = image_shape,
                           kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
@@ -219,7 +219,7 @@ exif1,exif2= image_exif('D02_img_orig_0001.jpg','D01_img_orig_0001.jpg')
 #print(len(label))
 labels=[]
 somma=0
-print(exif_lbl)
+#print(exif_lbl)
 for i in range(len(exif_lbl)):
      for j in range(len(exif_lbl[0])):
          somma = exif_lbl[0][j]+somma
@@ -227,7 +227,7 @@ for i in range(len(exif_lbl)):
             somma=somma+randint(0, 1)
      labels.append(somma)
      somma=0
-print(labels)
+#print(labels)
 # extract exif data
 # dict,dict_keys = extract_exif()
 # generate labels for each pair of images
