3d88f30fd03a07175bdc720062005b3e68aedddc
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-20 12:07:31 +0200

    mk

--- prova_exif.py
+++ prova_exif.py
@@ -116,15 +116,15 @@ def create_siamese_model(image_shape, dropout_rate):
     
     L1_layer = Lambda(lambda tensors: tf.abs(tensors[0] - tensors[1]))
     L1_distance = L1_layer([output_left, output_right])
-    L1_prediction = Dense(1, use_bias=True,
+    L1_prediction = Dense(71, use_bias=True,
                           activation='sigmoid',
                           input_shape = image_shape,
                           kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
                           name='weighted-average')(L1_distance)
-
-    prediction = Dropout(0.2)(L1_prediction)
-    out= np.zeros(71)
-    siamese_model = Model(inputs=[input_left, input_right], outputs= out)
+    prediction = Flatten(name=flatten_name)(L1_prediction)
+    #prediction = Dropout(0.2)(L1_prediction)
+    
+    siamese_model = Model(inputs=[input_left, input_right], outputs= prediction)
 
     return siamese_model
 
