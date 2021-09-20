1f53ba7310fc5896b371231c5b43823fd49dd5b8
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-20 11:24:34 +0200

    m

--- prova_exif.py
+++ prova_exif.py
@@ -121,9 +121,9 @@ def create_siamese_model(image_shape, dropout_rate):
                           kernel_initializer=RandomNormal(mean=0.0, stddev=0.001),
                           name='weighted-average')(L1_distance)
 
-    prediction = Dropout(0.2)(L1_prediction)
+    #prediction = Dropout(0.2)(L1_prediction)
 
-    siamese_model = Model(inputs=[input_left, input_right], outputs=prediction)
+    siamese_model = Model(inputs=[input_left, input_right], outputs=L1_prediction)
 
     return siamese_model
 """
