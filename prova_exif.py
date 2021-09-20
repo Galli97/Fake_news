2d4f420f3fbc38079577df037b0f8beddd1e164f
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-20 12:09:35 +0200

    ,n

--- prova_exif.py
+++ prova_exif.py
@@ -98,7 +98,7 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
     flatten_name = 'flatten' + str(suffix)
 
     x = model.output
-    x = Flatten(name=flatten_name)(x)
+    #x = Flatten(name=flatten_name)(x)
     x = Dense(4096, activation='relu')(x)
     x = Dropout(dropout_rate)(x)
 
