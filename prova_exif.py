dc537db497e66f8bbd6f0f3c007e04f5d94e86fb
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-21 15:21:37 +0200

    bjk

--- prova_exif.py
+++ prova_exif.py
@@ -34,7 +34,8 @@ def fix_gpu():
 
 fix_gpu()
 
-gpus = tf.config.experimental.list_physical_devices('GPU') tf.config.experimental.set_memory_growth(gpus[0], True)
+gpus = tf.config.experimental.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
 def random_list(list):
     second_list = []
