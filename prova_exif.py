243935c3384d48e61e6c25e08b3551249366ceaa
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-21 14:59:06 +0200

    dvz

--- prova_exif.py
+++ prova_exif.py
@@ -21,6 +21,12 @@ from random import randint
 import random
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
+
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
 def random_list(list):
     second_list = []
     for i in range(len(list)):
