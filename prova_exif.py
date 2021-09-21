7ec3bc8a128e992b4445f5d01ed667b5bab4fd46
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-21 15:18:33 +0200

    bjk

--- prova_exif.py
+++ prova_exif.py
@@ -199,4 +199,4 @@ image2=tf.stack(images2,axis=0)
 #label =  tf.compat.v1.placeholder(np.zeros(71), [None, 71])
 
 
-siamese_model.fit(datagenerator((list1[0],list2[0]),labels,batchsize=16, mode="train"),epochs=10)
+siamese_model.fit(datagenerator((list1,list2),exif_lbl,batchsize=64, mode="train"),epochs=10)
