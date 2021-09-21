b81d2de2386bf4a7111d72efa7656684bea48b61
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-21 11:05:13 +0200

    vjh

--- prova_resnet.py
+++ prova_resnet.py
@@ -79,7 +79,7 @@ def create_siamese_model(image_shape, dropout_rate):
     # Create the model
     model2 = Sequential()
     #model2.add(Dense(8192, input_shape=output_siamese_shape, activation='relu'))
-    model2.add(Dense(4096, input_shape=output_siamese_shape,activation='relu'))
+    model2.add(Dense(4096, input_shape=output_siamese_shape.shape,activation='relu'))
     model2.add(Dense(2048, activation='relu'))
     model2.add(Dense(1024, activation='relu'))
     model2.add(Dense(num_classes, activation='softmax'))
@@ -102,7 +102,7 @@ def create_mlp_model(output_siamese_shape):
     # Create the model
     model2 = Sequential()
     #model2.add(Dense(8192, input_shape=output_siamese_shape, activation='relu'))
-    model2.add(Dense(4096, input_shape=output_siamese_shape.shape,activation='relu'))
+    model2.add(Dense(4096, input_shape=output_siamese_shape,activation='relu'))
     model2.add(Dense(2048, activation='relu'))
     model2.add(Dense(1024, activation='relu'))
     model2.add(Dense(num_classes, activation='softmax'))
