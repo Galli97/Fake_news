4164e0f75fb56e1a2df46b2c8f0d3b1ef28ce879
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-21 11:03:08 +0200

    xzckbj

--- prova_resnet.py
+++ prova_resnet.py
@@ -75,7 +75,7 @@ def create_siamese_model(image_shape, dropout_rate):
     
     output_siamese_shape = tf.concat([output_left,output_right],1)
     print('Dimensione in output')
-    print(output.shape)
+    #print(output.shape)
     # Create the model
     model2 = Sequential()
     #model2.add(Dense(8192, input_shape=output_siamese_shape, activation='relu'))
