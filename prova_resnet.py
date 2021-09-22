3c924155b08e5cad09018392e30fc40dc067cd17
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-22 13:21:51 +0200

    aC

--- prova_resnet.py
+++ prova_resnet.py
@@ -73,7 +73,7 @@ def create_siamese_model(image_shape, dropout_rate):
 
     
     output_left, input_left, model = create_base_model(image_shape, dropout_rate)
-    output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
+    output_right, input_right, model = create_base_model(image_shape, dropout_rate, suffix="_2")
     
     output_siamese = tf.concat([output_left,output_right],1)
     num_classes=71;
