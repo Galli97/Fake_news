00ca91698a95c863bf81c0b9ffcd54b601016b06
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-09-27 18:48:35 +0200

    sac

--- prova_resnet.py
+++ prova_resnet.py
@@ -125,7 +125,7 @@ def create_mlp(image_shape,dropout_rate):
 
 
 
-total_model=create_mlp(image_shape=(128,128,3),dropout_rate=0.2)
+total_model=create_mlp(image_shape=(128,128,3),dropout_rate=0.1)
 
 total_model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.01), metrics=['accuracy'])
 
