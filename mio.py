75199440ef4a74f681987fb9d912ebc2bfd615d7
Galli97 <galli.1753274@studenti.uniroma1.it>
2021-10-05 16:26:31 +0200

    bkj

--- mio.py
+++ mio.py
@@ -62,7 +62,7 @@ def create_base_model(image_shape, dropout_rate, suffix=''):
     flatten_name = 'flatten' + str(suffix)
 
     x = model.output
-    x = Dense(128, activation='relu')(x)
+    x = Dense(256, activation='relu')(x)
     x = Flatten(name=flatten_name)(x)
     
 
@@ -76,7 +76,7 @@ def create_siamese_model(image_shape, dropout_rate):
     output_right, input_right = create_base_model(image_shape, dropout_rate, suffix="_2")
     
     output_siamese = tf.concat([output_left,output_right],1)
-    num_classes=71;
+    num_classes=45;
     
     x = output_siamese
     x = Dense(4096, activation='relu')(x)
